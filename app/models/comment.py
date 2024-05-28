271f92808f9c3221a80441941fc59d58fe14569f
boak123 <boluwatifeakorede17@gmail.com>
2024-05-28 10:02:35 +0000

    comment models

---
+++ app/models/comment.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy
+
+"""Comment model"""
+class Comment( db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
